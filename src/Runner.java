18acd6b45d20974faf86a85049122717009ed1b9
Mahmut Ugurlu <mahmutugurlu0718@gmail.com>
2025-01-03 20:27:23 +0100

    v1

---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+        //versiyon 1
+
+        System.out.println("degisiklik 1");// suan working space de
+
+        //git add komutu ile staging area bekleme alanina aliriz
+
+        //git status komutu ile calismamizin hangi alanda oldugunu kontrol edebiliriz.
+
+
+
+
+    }
+}
