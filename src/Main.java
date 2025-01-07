f3e998d7cbff9f1d8ab9ed29869304a6e78e03f6
Mahmut Ugurlu <mahmutugurlu0718@gmail.com>
2025-01-07 14:53:39 +0100

    v7

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,42 @@ public class Main {
 
 
 
+        //git add komutu ile staging area bekleme alanina aliriz
+
+        //git status komutu ile calismamizin hangi alanda oldugunu kontrol edebiliriz.
+
+        //git log komutu commit store alanda yer alan bütün commitleri gösterir,
+        // kim , ne zaman yapmis görebiliriz
+
+        //git log --oneline tüm commitlerimi kisa olarak listeler
+
+        // git show (Hash kod) yapilan bir degisikligi görebiliriz
+        // kim ne zaman nerde degisiklik yapmis görebiliiriz.
+
+        //git restore . veya dosya adi kullanarak working spacedeki degisiklikleri silebiliriz.
+
+        // git restore --staged . veya dosya adiyla staging alanindaki (git add. den sonra)
+        // degisiklikleri siler
+
+        // git reset --hard komutu degisiklikleri working space ve staging areadan komple siler
+        //dikkatli kullanmak lazim geri dönüsü yoktur.
+
+        //  git commit -am"verilecek mesaj" bu kisa yolla git add ve git commit -m  komitlerini beraber kullanmis oluruz
+
+        // git checkout (hash) komutu ile incelemek istedigimz versiyona gideriz
+
+        //git reset --hard (hash) eski versiyonlara geri götürür ama degisiklikleri siler
+
+        // git remote add origin + repo adresi githubla lokalimizdeki git reposu irtibat kurmasini saglar
+
+        // ilk defa push yapacaksak ilk önce  git push --set-upstream origin master veya  git push -u
+        //komutlariyla push yapmaliyiz
+
+
+
+
+
+
 
     }
 }
